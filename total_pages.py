59f911abf0afd010bed75961ecaaa51371053874
YangHuiS <104430713+YangHuiS@users.noreply.github.com>
2022-05-28 19:23:23 +0800

    Update total_pages.py

--- total_pages.py
+++ total_pages.py
@@ -11,33 +11,12 @@ st.set_page_config(
 )
 
 #加载背景图（本地图片先转url，网页图片就直接给图片的链接）
-img_url = image_to_url('back1.png', width=-3, clamp=False,
-                       channels='RGB', output_format='auto', image_id='',
-                       allow_emoji=False)
+img_url = 'https://img-blog.csdnimg.cn/d1dd8bb83096492c9891b64267dba6e7.png'
 # 通过markdown加载背景图（可以是动图、静图）
 st.markdown('''
 <style>
 .css-fg4pbf {background-image: url(''' + img_url + ''');}</style>
 ''', unsafe_allow_html=True)
-
-# %% ----------会话状态----------
-if 'first_visit' not in st.session_state:
-    st.session_state.first_visit = True
-else:
-    st.session_state.first_visit = False
-# # 初始化全局配置
-# if st.session_state.first_visit:
-#     st.session_state.date_time = datetime.datetime.now() + datetime.timedelta(hours=8)  # Streamlit Cloud的时区是UTC，加8小时即北京时间
-#     # with open('./back.mp4', 'rb') as f:
-#     #     au = f.read()
-#     # t1 = datetime.datetime.now()
-#     # st.video(au)
-#     # st.write('''<style>
-#     # #video {controls="controls" autoplay="autoplay" loop}</style>''', unsafe_allow_html=True)
-#     # if datetime.datetime.now() - t1 == 20:
-#     #     st.stop()
-#     st.snow()
-#     st.balloons()
 
 
 app = mt.MultiPage()
