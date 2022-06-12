1d1a743baf3f2faf90747cc0dbf8ae7d5239dbdf
YangHuiS <104430713+YangHuiS@users.noreply.github.com>
2022-06-12 10:51:04 +0800

    Update page_a.py

--- pages/page_a.py
+++ pages/page_a.py
@@ -488,10 +488,10 @@ def main():
 #             ' + '.join([f'{j} * x_{i + 1}' for i, j in
 #                         enumerate(np.round(model.coef_, 2))]) + f' + {np.round(model.intercept_, 2)}'
 #         )
-        t = ' + '.join([f'{j} * x_{i + 1}' for i, j in enumerate(np.round(model.coef_, 2))]) + f' + {np.round(model.intercept_, 2)}'
+        t =  ' + '.join([f'{j} * x_{i + 1}' for i, j in enumerate(np.round(model.coef_, 2))]) + f' + {np.round(model.intercept_, 2)}'
         st.markdown(
             # f'权重：{np.round(model.coef_, 2)}  \n\n 偏置项：{model.intercept_:.2f}'
-            '$$y = {t}$$'
+            f'$$y = {t}$$'
         )
 
 
